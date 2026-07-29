174447e3532effe4e77549484f1fe60cff044cf1
dhanush2468dd-web <dhanush2468dd@gmail.com>
2026-07-28 23:08:29 -0700

    const: variable which cannot be reassignned after its declaration

--- Day/Day 8/P4.js
+++ Day/Day 8/P4.js
@@ -1,23 +1,10 @@
-
-var x = 10;
-var x = 20;
-x = 30;
-console.log(x)
-
-if (true) {
-var message = "Hello";
+if(true){
+let secret = "Password@123"; 
+console.log(secret); 
 }
-
-console.log(message);
-console.log(message);
-
-let score = 100;
-
-score = 160; 
-console.log(score)
 
 if(true){
-    let secret = "Password@123"; 
+    const pi = 3.14159;
 
-console.log(secret);
-}
+    console.log(pi);
+}   
