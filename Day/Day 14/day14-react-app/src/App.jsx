89942f580cde17df1088c1d517ec2cf571ae28bc
dhanush2468dd-web <dhanush2468dd@gmail.com>
2026-07-31 23:30:06 -0700

    FetchingData ex3

--- Day/Day 14/day14-react-app/src/App.jsx
+++ Day/Day 14/day14-react-app/src/App.jsx
@@ -10,6 +10,8 @@ import SignupForm from "./components/FormTwo";
 import ValidatedForm from "./components/FormThree";
 import GitHubUsersList from "./components/FetchingData";
 import GitHubProfile from "./components/FetchingDataTwo";
+import GitHubRepoCount from "./components/FetchingDataThree";
+
 function App() {
 
 return (
@@ -23,10 +25,11 @@ return (
 {/* <ThemeProviderDemo /> */}
 {/* <SimpleNameForm /> */}
 {/* <SignupForm /> */}
-      <ValidatedForm/>
-      {/* <ValidatedForm/> */}
-      {/* <GitHubUsersList/> */}
+{/* <ValidatedForm/> */}
+{/* <GitHubUsersList/> */}
       <GitHubProfile/>
+      {/* <GitHubProfile/> */}
+      <GitHubRepoCount username="rakeshdotkr" />
 </>
 )
 }
