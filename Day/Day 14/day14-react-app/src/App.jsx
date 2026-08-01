7b8b23960ddd961da23786601056a7c0b68a4121
dhanush2468dd-web <dhanush2468dd@gmail.com>
2026-07-31 23:24:56 -0700

    Form ex2

--- Day/Day 14/day14-react-app/src/App.jsx
+++ Day/Day 14/day14-react-app/src/App.jsx
@@ -6,22 +6,22 @@ import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
 import SimpleNameForm from "./components/FormOne";
+import SignupForm from "./components/FormTwo";
+
 
 function App() {
-
 return (
-<>
-{/* <SimpleList />
-     <UserList />
-     <ProductListDemo />
-
-     <CounterHook/> */}
-{/* <UserListHook/> */}
-      <ThemeProviderDemo />
-      {/* <ThemeProviderDemo /> */}
-      <SimpleNameForm />
-</>
+  <>
 )
+     <CounterHook/> 
+<UserListHook/> 
+ <ThemeProviderDemo /> 
+  <SimpleNameForm />
+ <SimpleNameForm /> 
+      <SignupForm />
+</>
+);
+
 }
 
-export default App
+export default App;
